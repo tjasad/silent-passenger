e9015732b4b95d71b501657915bcf80f8bbd06b2
quarkusio <no-reply@quarkus.io>
2021-05-22 09:13:48 +0000

    Initial commit

---
+++ src/test/java/si/red/dragons/NativeGreetingResourceIT.java
@@ -0,0 +1,9 @@
+package si.red.dragons;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeGreetingResourceIT extends GreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
